5eb94154d75295b4cc6f015a2a9d4af124770edd
BhargavRaseshPatel <A00303747@canadahanson.ca>
2026-09-19 15:29:25 -0400

    correct the bug

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -7,12 +7,15 @@ import { useRouter } from 'next/navigation';
 export default function Home() {
 
   const router = useRouter();
-  
+
   useEffect(() => {
     const token = localStorage.getItem("token");
 
     if (token) {
       router.push("/dashboard");
+    }
+    else {
+      router.push("/auth-screen")
     }
   }, []);
   return (
