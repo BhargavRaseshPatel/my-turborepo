9b7d1454a8a2d8548bed779bbb51fa24eff0c155
BhargavRaseshPatel <A00303747@canadahanson.ca>
2026-09-19 15:17:06 -0400

    Revert "once the user login in, then redirect to the dashboard"
    This reverts commit 605ddfe927480544641eb135d6309fdab0a91e69.

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,22 +1,6 @@
-"use client"
 import Image from "next/image";
-import { useEffect } from "react";
-import { useRouter } from 'next/navigation';
-
 
 export default function Home() {
-
-  const token = localStorage.getItem('token')
-  const router = useRouter();
-
-
- useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      router.push("/dashboard");
-    }
-  }, [router]);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
